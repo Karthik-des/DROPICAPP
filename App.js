a5329bb9a5ab700d2bd7f9413cbf2914f23a19b5
V karthik <karthikvenkatesh7258@gmail.com>
2025-09-10 14:01:52 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Video } from 'expo-av';
+import carVideo from './assets/india3.mp4';
+import styles from './src/styles/AppCss';
+
+
+const Stack = createStackNavigator();
 
 export default function App() {
+  const [startingValue, setStartingValue] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setStartingValue(false), 2500);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (startingValue) {
+    return (
+      <View style={{ flex: 1 }}>
+        <View style={styles.container}> 
+          <View style={styles.home}>
+            <Video
+              source={carVideo}
+              rate={1.0}
+              volume={1.0}
+              isMuted={false}
+              resizeMode="contain"
+              shouldPlay
+              isLooping
+              style={{ width: '200%', height: 700 }}
+            />
+            <StatusBar style="auto" />
+          </View>
+        </View>
+      </View>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      {/* <Stack.Navigator initialRouteName="OrderScreen">
+
+       
+        <Stack.Screen name="OrderScreen" component={OrderScreen} />
+       
+      </Stack.Navigator> */}
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
